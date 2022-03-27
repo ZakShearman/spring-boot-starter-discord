9f5add80639c4ed4be726b52161c0e525984255d
Zak <shearmanzak@gmail.com>
2022-03-27 05:45:35 +0100

    remove springframework plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     `maven-publish`
 
     id("io.freefair.lombok") version "6.4.1"
-
-    id("org.springframework.boot") version "2.6.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
@@ -19,7 +17,7 @@ dependencies {
     implementation("net.dv8tion:JDA:5.0.0-alpha.9")
     implementation("com.google.code.gson:gson:2.9.0")
 
-    compileOnly("org.springframework.boot:spring-boot-starter-data-jpa")
+    compileOnly("org.springframework.boot:spring-boot-starter-data-jpa:2.6.5")
 }
 
 tasks.getByName<Test>("test") {
